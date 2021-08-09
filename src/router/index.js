3b4962d7ec4cb51a62bf42712cb347980786d097
Mayara Pimenta <mayaralrpimenta@gmail.com>
2021-08-09 15:47:15 -0300

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import EventCreate from "../views/EventCreate.vue";
+import EventList from "../views/EventList.vue";
+import EventShow from "../views/EventShow.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "event-list",
+    component: EventList,
+  },
+  {
+    path: "/event",
+    name: "event-show",
+    component: EventShow,
+  },
+  {
+    path: "/event-create",
+    name: "event-create",
+    component: EventCreate,
+  }, 
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
---
+++ src/views/EventCreate.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Create Events</h1>
+</template>
