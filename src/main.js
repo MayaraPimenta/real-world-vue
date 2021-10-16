977716d128ac4194fcd19164981277b966a4cd35
Mayara Pimenta <mayara@primelan.com.br>
2021-10-16 14:53:18 -0300

    starting progress bar

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "nprogress/nprogress.css";
 
 import upperFirst from "lodash/upperFirst";
 import camelCase from "lodash/camelCase";
